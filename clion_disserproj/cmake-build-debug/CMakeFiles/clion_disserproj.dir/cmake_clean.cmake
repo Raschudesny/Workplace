2c3cc1e4391ac0cd7221a9d41267c4c0cd931255
Raschudesny <rifkinsm@gmail.com>
2018-03-29 18:55:32 +0300

    initproj commit

---
+++ clion_disserproj/cmake-build-debug/CMakeFiles/clion_disserproj.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/clion_disserproj.dir/main.c.o"
+  "clion_disserproj.pdb"
+  "clion_disserproj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/clion_disserproj.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
